564c4372223ccf22f88e025223c984f968b2773c
VanthuirMaia <vanmaiasf@gmail.com>
2025-03-31 22:47:55 -0300

    Correção Bug Error 500

--- backend/crud.py
+++ backend/crud.py
@@ -8,7 +8,7 @@ def get_products(db: Session):
 
 # get where id = 1
 def get_product(db: Session, product_id: int):
-    return db.query(ProductModel).filter(ProductModel.id == product_id)
+    return db.query(ProductModel).filter(ProductModel.id == product_id).first()
 
 # insert into (create)
 def create_product(db: Session, product: ProductCreate):
@@ -26,25 +26,18 @@ def delete_product(db: Session, product_id: int):
 
 def update_product(db: Session, product_id: int, product: ProductUpdate):
     db_product = db.query(ProductModel).filter(ProductModel.id == product_id).first()
-
     if db_product is None:
         return None
-    
     if product.name is not None:
         db_product.name = product.name
-
     if product.description is not None:
         db_product.description = product.description
-
     if product.price is not None:
         db_product.price = product.price
-    
     if product.categoria is not None:
         db_product.categoria = product.categoria
-
     if product.email_fornecedor is not None:
         db_product.email_fornecedor = product.email_fornecedor
-
     db.commit()
     db.refresh(db_product)
     return db_product
--- backend/router.py
+++ backend/router.py
@@ -15,16 +15,16 @@ router = APIRouter()
 
 # Criar a rota de buscar todos os itens
 @router.get("/products", response_model=List[ProductResponse])
-def read_all_products(db: Session = Depends(get_db)):
+def read_all_products_route(db: Session = Depends(get_db)):
     """
     Essa é minha rota de ler todos os produtos do banco de dados
     """
-    products = get_product(db)
+    products = get_products(db)
     return products
 
 # Criar a rota de buscar 1 item
 @router.get("/products/{product_id}", response_model=ProductResponse)
-def read_one_product(product_id: int, db: Session = Depends(get_db)):
+def read_one_product_route(product_id: int, db: Session = Depends(get_db)):
     """
     Essa é minha rota de apenas 01 produto do banco de dados
     """
@@ -35,32 +35,32 @@ def read_one_product(product_id: int, db: Session = Depends(get_db)):
 
 # Criar a rota de adicionar um item
 @router.post("/products/", response_model=ProductResponse)
-def create_product(product: ProductCreate, db: Session = Depends(get_db)):
+def create_product_route(product: ProductCreate, db: Session = Depends(get_db)):
     """
     Essa é minha rota de criar um produto do banco de dados
     """
-    return create_product(product=product, db=db)
+    return create_product(db=db, product=product)
 
 # Criar a rota de deletar um item
 @router.delete("/products/{product_id}", response_model=ProductResponse)
-def delete_product(product_id: int, db: Session = Depends(get_db)):
+def delete_product_route(product_id: int, db: Session = Depends(get_db)):
     """
     Essa é minha rota de apagar um produto do banco de dados
     """
-    product_db = delete_product(product_id=product_id, db=db)
-    if product_db is None:
+    db_product = delete_product(db, product_id=product_id)
+    if db_product is None:
         raise HTTPException(status_code=404, detail="Esse produto não existe")
-    return delete_product(product_id=product_id, db=db)
+    return db_product
 
 # Criar a rota de Editar um item
 @router.put("/products/{product_id}", response_model=ProductResponse)
-def atualizar_product(product_id: int, product: ProductResponse, db: Session=Depends(get_db)):
+def update_product_route(product_id: int, product: ProductResponse, db: Session=Depends(get_db)):
     """
     Essa é minha rota de editar um produto do banco de dados
     """
-    product_db = update_product(db=db, product_id=product_id, product=product)
-    if product_db is None:
+    db_product = update_product(db, product_id=product_id, product=product)
+    if db_product is None:
         raise HTTPException(status_code=404, detail="Esse produto não existe")
-    product_db
+    return db_product
     
     
